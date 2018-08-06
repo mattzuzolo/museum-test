d461fcb1ba5167d1e45f4d5b4f5d7bfd6e912cf1
github username <github email address>
2018-08-06 01:03:14 -0400

    click now posting to DB

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 console.log("js is loaded!!!");
 
+const clickUrl = "https://shrouded-anchorage-38777.herokuapp.com/clicks"
+
 let searchQuery = ""
 const artContainer = document.getElementById("artwork-container")
 
 let mousePosition = {
-    xVal: "",
-    yVal: ""
+    xLocation: "",
+    yLocation: ""
 }
 
 var apiEndpointBaseURL = "https://api.harvardartmuseums.org/object";
 var queryString = $.param({
-    apikey: "API KEY HERE",
-    person: "Joan Miro",
+    apikey: "API_KEY",
+    person: "Van Gogh",
     classification: "Paintings"
 });
 
@@ -29,13 +31,15 @@ $.getJSON(apiEndpointBaseURL + "?" + queryString, function(data) {
       let myElement = document.createElement('div');
       artContainer.append(myElement)
 
-      mousePosition.xVal = event.clientX
-      mousePosition.yVal = event.clientY
+      mousePosition.xLocation = event.clientX
+      mousePosition.yLocation = event.clientY
 
       myElement.id = 'my-element';
       document.getElementById('my-element').style.position = "fixed";
-      document.getElementById('my-element').style.top = mousePosition.yVal +"px";
-      document.getElementById('my-element').style.left = mousePosition.xVal +"px";
+      document.getElementById('my-element').style.top = mousePosition.yLocation +"px";
+      document.getElementById('my-element').style.left = mousePosition.xLocation +"px";
+
+      fetchPost(mousePosition);
 
 })
 
@@ -44,6 +48,21 @@ $.getJSON(apiEndpointBaseURL + "?" + queryString, function(data) {
 });
 
 
+function fetchPost(body){
+  console.log("entered POST function")
+
+  const postConfig = {
+    Accept: "application/json",
+    method: "POST",
+    headers: {
+         "Content-type": "application/json"
+    },
+    body: JSON.stringify(body)
+  };
+
+  console.log("Fetch should trigger now.")
+  return fetch(clickUrl, postConfig)
+}
 
 
 // title: "rabbit",
